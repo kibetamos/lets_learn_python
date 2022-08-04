11d065e0e785e8621cc74383f8e7870c98a77e04
kibet kilel <amos.rotich@strathmore.edu>
2022-08-04 12:09:57 +0300

    Update: Add funtionality to the BST file

--- BST/bst.py
+++ BST/bst.py
@@ -30,4 +30,35 @@ class BinaryTreeSearch:
             return search(root.leftChild, value)
         else:
             return search(root.rightChild,value)
+
+#Problem - Get the height of the 
+#The height of a binary search tree is the number 
+# of edges between the tree's root and its furthest leaf
+    def getHeight(self,root):
+        #Write your code here
+        return -1 if root is None else 1 + max(self.getHeight(root.left),
+        self.getHeight(root.right))
+
+    #Find the smallest element
+
+    def findSmallestElement(root):
+        #Lets check if the binary tree is empty
+        if root == None:
+            return False
+            #Check if the current node is the left most
+        elif root.leftChild==None:
+            return root.data
+        #Lets check the right subtree of current node
+        else:
+            return findSmallestElement(root.leftChild)
+            
+    def findLargestElement(root):
+        if root ==None:
+            return False
+        elif root.rightChild == None:
+            return root.data
+        else:
+            return findLargestElement(root.rightChild)
+
+            
         
