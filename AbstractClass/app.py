22991e1ea68a5c72d1220a8f03d42d23bf96ae17
Amos <33954664+kibetamos@users.noreply.github.com>
2023-06-01 17:37:03 +0300

    Update app.py

--- AbstractClass/app.py
+++ AbstractClass/app.py
@@ -2,12 +2,17 @@ from fulltimeemployee import FulltimeEmployee
 from hourlyemployee import HourlyEmployee
 from payroll import Payroll
 
+# Create a Payroll object
 payroll = Payroll()
 
+# Add FulltimeEmployee instances to the payroll
 payroll.add(FulltimeEmployee('John', 'Doe', 6000))
 payroll.add(FulltimeEmployee('Jane', 'Doe', 6500))
+
+# Add HourlyEmployee instances to the payroll
 payroll.add(HourlyEmployee('Jenifer', 'Smith', 200, 50))
 payroll.add(HourlyEmployee('David', 'Wilson', 150, 100))
 payroll.add(HourlyEmployee('Kevin', 'Miller', 100, 150))
 
+# Print the payroll information
 payroll.print()
