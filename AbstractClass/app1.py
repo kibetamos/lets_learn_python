86dc0250f8744d2b4f1657c5b31131af453456b2
Amos <33954664+kibetamos@users.noreply.github.com>
2023-06-01 17:39:12 +0300

    Update app1.py

--- AbstractClass/app1.py
+++ AbstractClass/app1.py
@@ -1,11 +1,16 @@
-from example1 import Full_time_employee,part_time_employee,payroll
+from example1 import Full_time_employee, part_time_employee, payroll
 
-payroll = payroll()
+# Create a Payroll object
+payroll_obj = payroll()
 
-payroll.add(Full_time_employee('John', 'Doe', 6000))
-payroll.add(Full_time_employee('Jane', 'Doe', 6500))
-payroll.add(part_time_employee('Jenifer', 'Smith', 200, 50))
-payroll.add(part_time_employee('David', 'Wilson', 150, 100))
-payroll.add(part_time_employee('Kevin', 'Miller', 100, 150))
+# Add Full_time_employee instances to the payroll
+payroll_obj.add(Full_time_employee('John', 'Doe', 6000))
+payroll_obj.add(Full_time_employee('Jane', 'Doe', 6500))
 
-payroll.print()
+# Add part_time_employee instances to the payroll
+payroll_obj.add(part_time_employee('Jenifer', 'Smith', 200, 50))
+payroll_obj.add(part_time_employee('David', 'Wilson', 150, 100))
+payroll_obj.add(part_time_employee('Kevin', 'Miller', 100, 150))
+
+# Print the payroll information
+payroll_obj.print()
