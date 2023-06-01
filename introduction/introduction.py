751b9cc09b97cb3756728b1fa98448d4d5105b98
Amos <33954664+kibetamos@users.noreply.github.com>
2023-06-01 17:32:30 +0300

    Update introduction.py
    added afew changes

--- introduction/introduction.py
+++ introduction/introduction.py
@@ -1,7 +1,12 @@
 """
 A class in python is defined by the use of a class keyword
-E.g class Person:
-        pass
+E.g 
+
+class Person:
+    pass
+
+person = Person()
+
 
 classes are callable
 we can create an object from from a class.
@@ -18,6 +23,9 @@ class Person:
     def __init__(self,age,name):
         self.name =name
         self.age =age
+In this case, the __init__ method takes two parameters (age and name),
+and it initializes the age and name attributes of the object using the values passed to the constructor.
+
 
 Instance attributes are initialized by the __init__ method, the self is the
 self is the instance of the person class.
