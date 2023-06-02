c41f848db7ae1c4f58b3d411a48160c7d9994f91
Amos <33954664+kibetamos@users.noreply.github.com>
2023-06-02 19:50:59 +0300

    Update example1.py
    added afew more stringfunctons

--- strings/example1.py
+++ strings/example1.py
@@ -8,3 +8,21 @@ print(''.join(["F", "B", "I"]))
 
 # print("smartphone".startswith("smart"))
 # Matches the string's prefix against the argument: True
+
+# Here are some of the most important string methods:
+
+#  1.   strip(): Removes leading and trailing whitespace from a string.
+
+y = " This is lazy\t\n "
+print(y.strip())  # Output: 'This is lazy'
+
+
+#2.  join(): Glues together all elements in a list using the separator string.
+
+print(''.join(["F", "B", "I"]))  # Output: 'FBI'
+
+
+# 3 . startswith(): Checks if a string starts with a specific prefix and returns a boolean value.
+
+print("smartphone".startswith("smart"))  # Output: True
+
