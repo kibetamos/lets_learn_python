d162ba2047f49d444b145f321f844d3e21060a16
Amos <33954664+kibetamos@users.noreply.github.com>
2023-06-02 19:38:38 +0300

    Update inheritance1.py

--- inheritance/inheritance1.py
+++ inheritance/inheritance1.py
@@ -7,5 +7,12 @@ class Person:
 
 class Employee(Person):
     def __init__(self, name, job_title):
+        # Call the __init__ method of the Person class to initialize the name attribute
         super().__init__(name)
         self.job_title = job_title
+
+# Create an instance of the Employee class
+employee = Employee("John Doe", "Software Engineer")
+print(employee.name)  # Output: John Doe
+print(employee.job_title)  # Output: Software Engineer
+print(employee.greet())  # Output: Hi, it's John Doe
