f15a06b19f44d596d0be732c16c25fd9b63b1cab
Amos <33954664+kibetamos@users.noreply.github.com>
2023-06-07 20:04:03 +0300

    Update inheritance1.py
    made changes on the inheritance file

--- inheritance/inheritance1.py
+++ inheritance/inheritance1.py
@@ -1,3 +1,21 @@
+# class Person:
+#     def __init__(self, name):
+#         self.name = name
+
+#     def greet(self):
+#         return f"Hi, it's {self.name}"
+
+# class Employee(Person):
+#     def __init__(self, name, job_title):
+#         # Call the __init__ method of the Person class to initialize the name attribute
+#         super().__init__(name)
+#         self.job_title = job_title
+
+# # Create an instance of the Employee class
+# employee = Employee("The Ark", "Software Engineer")
+# print(employee.name)  # Output: John Doe
+# print(employee.job_title)  # Output: Software Engineer
+# print(employee.greet())  # Output: Hi, it's John Doe
 class Person:
     def __init__(self, name):
         self.name = name
@@ -7,12 +25,15 @@ class Person:
 
 class Employee(Person):
     def __init__(self, name, job_title):
-        # Call the __init__ method of the Person class to initialize the name attribute
         super().__init__(name)
         self.job_title = job_title
+
+    def get_details(self):
+        return f"Name: {self.name}, Job Title: {self.job_title}"
 
 # Create an instance of the Employee class
 employee = Employee("The Ark", "Software Engineer")
 print(employee.name)  # Output: John Doe
 print(employee.job_title)  # Output: Software Engineer
-print(employee.greet())  # Output: Hi, it's John Doe
+print(employee.greet())  # Output: Hi, it's The Ark
+print(employee.get_details())  # Output: Name: The Ark, Job Title: Software Engineer
