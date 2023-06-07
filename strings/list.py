fe2417b31e2a5092c3aa1cd7db6f4cd7d9c192d4
Amos <33954664+kibetamos@users.noreply.github.com>
2023-06-07 19:58:10 +0300

    Update list.py

--- strings/list.py
+++ strings/list.py
@@ -1,33 +1,33 @@
 '''
-We say that lists are mutable because you can modify
-them after creation.
-
+We say that lists are mutable because you can modify them after creation.
 '''
-#Adding Elements
-#Ways to add elements to anexisting list
+
+# Adding Elements
+# Ways to add elements to an existing list
 '''
 append, insert, or list concatenation.
-
 '''
+
 # 1. Append
 l = [1, 2, 2]
-'''append operation is the fastest 
-because it neither has to traverse the list to insert 
-an element at the correct position'''
 l.append(4)
 print(l)
-# [1, 2, 2, 4]
+# Output: [1, 2, 2, 4]
+
 # 2. Insert
 l = [1, 2, 4]
 l.insert(2, 3)
 print(l)
-# [1, 2, 3, 4]
-# 3. List Concatenation
-print([1, 2, 2] + [4])
-# [1, 2, 2, 4]
+# Output: [1, 2, 3, 4]
 
-#Reversing Lists
+# 3. List Concatenation
+l = [1, 2, 2]
+l += [4]
+print(l)
+# Output: [1, 2, 2, 4]
+
+# Reversing Lists
 l = [1, 2, 2, 4]
 l.reverse()
 print(l)
-# [4, 2, 2, 1]
+# Output: [4, 2, 2, 1]
